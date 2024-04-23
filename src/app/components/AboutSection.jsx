16efd16d22c330082b34736b757cfba915109f6d
maponbie <martha.aponbiede@valpo.edu>
2024-04-23 16:13:51 -0500

    Initial commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,149 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Valparaiso University, Valparaiso, Indiana</li>
+        <li>Bachelor of Science (BSc) with Magna Cum Laude honors</li>
+        <li>GPA: 3.75</li>
+        <li>January 2021 - May 2024</li>
+      </ul>
+    ),
+  },
+
+  {
+    title: "FrontEnd",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Tailwind CSS</li>
+        <li>JavaScript</li>
+        <li>HTML5</li>
+        <li>Flask</li>
+        <li>Git/GitHub/GitLab</li>
+        <li>Fast API</li>
+      </ul>
+    ),
+  },
+  
+  {
+    title: "BackEnd and More",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Python</li>
+        <li>Java</li>
+        <li>Spring Framework</li>
+        <li>C/C++</li>
+        <li>PostgreSQL</li>
+        <li>MongoDB</li>
+        <li>Neo4j</li>
+        <li>Large Language Models (LLMs)</li>
+        <li>Agile Methodology</li>
+        <li>Artificial Intelligence (AI)</li>
+      </ul>
+    ),
+  },
+
+  {
+    title: "Bootcamps and Hackathons",
+    id: "involvement",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Goldman Sachs x RTC Technical Bootcamp (November 2023 - April 2024)</li>
+        <li>Black Wings Hackathon (February 2024)</li>
+      </ul>
+    ),
+  },
+
+  {
+    title: "Awards",
+    id: "award",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Outstanding Leadership and Service Award (Valparaiso University)</li>
+        <li>Recipient of the 2024 Class's Top International Student Award (Valparaiso University)</li>
+        <li>Omicron Delta Kappa (Valpo Circle)</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+I'm a Software Engineer driven by a passion for crafting innovative solutions. My expertise spans Java, Python, JavaScript, React, Node.js, Express, PostgreSQL, HTML, CSS, Git, and many more. I thrive on continuous learning, always eager to broaden my knowledge and skills. A dedicated team player, I relish the opportunity to collaborate with others to deliver top-notch products.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Frontend{" "}
+            </TabButton>
+
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Backend and More{" "}
+            </TabButton>
+
+            <TabButton
+              selectTab={() => handleTabChange("involvement")}
+              active={tab === "involvement"}
+            >
+              {" "}
+              Bootcamps and Hackathons{" "}
+            </TabButton>
+
+            <TabButton
+              selectTab={() => handleTabChange("award")}
+              active={tab === "award"}
+            >
+              {" "}
+              Awards{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,133 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Nourish Net",
+    description: "Nourish Net is an intuitive platform aimed at addressing food insecurity among college students nationwide. Developed using React.js, Node.js. Team Size: 4",
+    image: "/images/projects/nourishNet.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/maponbie/DEVAS/tree/main/src",
+    previewUrl: "https://devpost.com/software/nourish-net-an-online-food-relief-network",
+  },
+
+  {
+    id: 2,
+    title: "Citation Application",
+    description: "TotlBib is a robust citation web application that enables users in effortlessly adding, searching, removing, and categorizing bibliography items. Developed using React.js, Node.js, Express.js, and PostgreSQL. Team Size: 3",
+    image: "/images/projects/citation.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/NicholasBean/Totl/tree/main/src",
+    previewUrl: "/",
+  },
+
+  {
+    id: 3,
+    title: "E-Commerce Website",
+    description: "Description: Development in Progress",
+    image: "/images/projects/5.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+
+  {
+    id: 4,
+    title: "Mapping Tracker Application",
+    description: "Description: Development in Progress",
+    image: "/images/projects/4.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+
+  {
+    id: 5,
+    title: "MS Paint (Java Rendition)",
+    description: "Employed Java and JavaFX to create a graphics editor, offering users vital tools to draw and edit images utilizing a pixel-based canvas. Team Size: 1",
+    image: "/images/projects/paint.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/maponbie/Martha-Paint/tree/master/src/main/java/com/example/paint",
+    previewUrl: "https://www.youtube.com/watch?v=9_dCrP5_5Ug",
+  },
+
+  {
+    id: 6,
+    title: "The Druid's Reckoning",
+    description: "The Druid's Reckoning, a 3D third-person RPG video game driven by Unreal Engine 5 (UE5), embodies an immersive gaming experience intricately developed through Agile methodologies and close collaboration with the storytelling team, vividly bringing the narrative to life. Team Size: 4",
+    image: "/images/projects/game.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/maponbie/3D-Game-Demo/tree/main",
+    previewUrl: "https://youtu.be/oBUhLqNEdJ4",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
